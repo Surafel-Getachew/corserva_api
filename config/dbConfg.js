87d726d2047a0fec542da564313e5444786bf754
surafel <surafel5751@gmail.com>
2023-02-26 15:25:05 +0300

    update test workflow ENV

--- config/dbConfg.js
+++ config/dbConfg.js
@@ -4,6 +4,7 @@ const env = process.env.NODE_ENV || 'development';
 dotenv.config({ path: `.env.${env}` });
 const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_DIALECT, DB_STORAGE } = process.env;
 
+
 module.exports = {
   username: DB_USERNAME,
   password: DB_PASSWORD,
--- db/connection.ts
+++ db/connection.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 import models from '../models';
 const dbConfig = require('../config/dbConfg');
+console.log("dbConfig",dbConfig);
 
 const connection = new Sequelize({
   ...dbConfig,
